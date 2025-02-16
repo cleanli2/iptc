92b53b9ae485a02bcdff95bfd6010494bb566e71
cleanli <clean_li@yahoo.com>
2025-02-16 20:09:39 +0800

    this way also do

--- b.sh
+++ b.sh
@@ -1,2 +1,3 @@
-g++ main.cpp -c -o main.o
-g++ -mwindows main.o -lgdi32 -luser32 -lkernel32 -lcomctl32 -o iptc.exe
+#g++ main.cpp -c -o main.o
+#g++ -mwindows main.o -lgdi32 -luser32 -lkernel32 -lcomctl32 -o iptc.exe
+g++ -mwindows main.cpp -o iptc.exe
